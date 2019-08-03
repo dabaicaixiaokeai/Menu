082b54c7b2ee71bc819196c5b0dbe00ed3b6848d
zysp <1525151236@qq.com>
2019-08-03 11:16:17 +0800

    fsdf

--- src/main/java/api/api.java
+++ src/main/java/api/api.java
@@ -1,4 +1,13 @@
 package api;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vo.Jsonresult;
+
+@RestController
 public class api {
+    @RequestMapping("/api/role/findpermission")
+    public Jsonresult findpermission(String Rolerid){
+        return new Jsonresult("200","500",null);
+    }
 }
